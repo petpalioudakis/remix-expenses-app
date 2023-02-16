3a26040b9af44bfe6fe81e58779086a558a19ed1
Petros Palioudakis <petros.palioudakis@mbww.com>
2023-02-16 11:30:43 +0200

    add dotenv

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,7 +4,8 @@ import { Response } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
-
+import { config } from 'dotenv'
+config({ path: '.env' })
 const ABORT_DELAY = 5000;
 
 export default function handleRequest(
